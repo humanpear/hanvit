620a0abab7369a7b407790ccb5d7556d871d7e35
human_pear <pibmaru@naver.com>
2026-03-24 09:59:35 +0900

    fix: 서치콘솔 meta데이터 추가 및 포트폴리오 페이지 스타일 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,19 @@ import "./globals.css";
 import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://hanvit-red.vercel.app/"),
+  metadataBase: new URL("https://interiorhv.com"),
   title: "한빛인테리어",
   description: "공간에 따뜻한 빛을 담는 한빛인테리어 입니다.",
+  verification: {
+    google: "google-site-verification=zRaI8bb_VHRH9hc37r5U3K9LorJMKGggoJp5mKl_3Go",
+    other: {
+      "naver-site-verification": "c0a08b2151155c706722aac169043e210cb4c5e5",      
+    }
+  },
   openGraph: {
     title: "한빛인테리어",
     description: "공간에 따뜻한 빛을 담는 한빛인테리어 입니다.",
-    url: "https://hanvit-red.vercel.app/",
+    url: "https://interiorhv.com",
     images: [
       {
         url: "/images/metaLogo.png",
