56034beedbde2a9b9506643dfdd888404ac7e41b
human_pear <pibmaru@naver.com>
2026-04-01 15:01:25 +0900

    fix: 검색엔진 최적화

--- components/sections/home.tsx
+++ components/sections/home.tsx
@@ -15,16 +15,21 @@ function Home() {
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-10 px-4 pb-14 pt-24 sm:px-6 md:flex-row md:justify-between md:gap-8 md:px-12 md:pb-40 md:pt-30 lg:px-20">
         <div className="flex w-full max-w-xl flex-col gap-8 md:gap-11">
           <div className="flex flex-col items-center md:items-start">
-            <div className="font-batang text-4xl font-bold pr-4 sm:text-6xl">자연을 담은</div>
+            <div className="font-batang text-4xl font-bold pr-4 sm:text-6xl">
+              자연을 담은
+            </div>
             <div className="font-batang text-4xl font-bold pl-4 italic text-wood-30 sm:text-6xl">
               따뜻한 공간
             </div>
           </div>
           <div className="flex flex-col gap-4 text-sm leading-relaxed sm:gap-5 sm:text-base">
-            <p>
-              한빛인테리어는 공간에 따뜻한 빛을 담아, 삶을 밝히는 새로운 시작을
-              제안합니다.
-            </p>
+            <div className="flex">
+              <h1>한빛인테리어</h1>
+              <p>
+                는 공간에 따뜻한 빛을 담아, 삶을 밝히는 새로운 시작을
+                제안합니다.
+              </p>
+            </div>
             <p>
               저희는 단순히 집을 꾸미는 일을 넘어, 고객 한 분 한 분의 생활에
               어울리는
